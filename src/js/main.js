a9f549de6dd1bcef5637fa999752ce36c7a09ac5
Maksym <lucikofficial@gmail.com>
2026-01-18 20:11:50 +0200

    Робив хедер

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 import "../scss/style.scss";
+
+const burger = document.getElementById("burger");
+const menu = document.getElementById("menu");
+const closeMenu = document.getElementById("close");
+
+burger.addEventListener("click", () => {
+  menu.classList.add("active");
+});
+
+closeMenu.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
